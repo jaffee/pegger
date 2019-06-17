cc57c3dc06ac56096bb7d9748911cd30240abf26
Matt Jaffee <matthew.jaffee@gmail.com>
2019-06-17 10:37:19 -0500

    use commandeer, config changes
    default concurrency to num cpu, expose profiling and iterations

--- main.go
+++ main.go
@@ -1,31 +1,57 @@
 package main
 
 import (
-	"flag"
-	"fmt"
+	"log"
 	"math/rand"
+	"net/http"
+	_ "net/http/pprof"
+	"runtime"
 	"sync"
+
+	"github.com/jaffee/commandeer"
 )
 
-func main() {
-	conc := flag.Int("conc", 8, "number of goroutines to spawn")
-	flag.Parse()
+type Main struct {
+	Concurrency int    `help:"Number of goroutines to spawn."`
+	Iterations  uint64 `help:"Number of times each routine should loop."`
+	Profiling   string `help:"Bind address for pprof."`
+}
+
+func NewMain() *Main {
+	return &Main{
+		Concurrency: runtime.NumCPU(),
+		Iterations:  1 << 40,
+		Profiling:   "localhost:6060",
+	}
+}
+
+func (m *Main) Run() error {
+	go func() {
+		log.Println(http.ListenAndServe(m.Profiling, nil))
+	}()
 
 	wg := sync.WaitGroup{}
-	vals := make([]int, *conc)
+	vals := make([]int, m.Concurrency)
 
-	for i := 0; i < *conc; i++ {
+	for i := 0; i < m.Concurrency; i++ {
 		vals[i] = rand.Int()
 		wg.Add(1)
 		go func(idx int) {
 			defer wg.Done()
 			rnd := rand.New(rand.NewSource(rand.Int63()))
-			for j := 0; j < 1000*1000*1000*1000; j++ {
+			for j := uint64(0); j < m.Iterations; j++ {
 				vals[idx] = vals[idx] ^ rnd.Int()
 			}
 		}(i)
 	}
 
 	wg.Wait()
-	fmt.Println(vals)
+	log.Println(vals)
+	return nil
+}
+
+func main() {
+	if err := commandeer.Run(NewMain()); err != nil {
+		log.Fatal(err)
+	}
 }
